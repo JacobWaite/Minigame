8d1750af6f0a1ee3da588560416f26e6e6505aa9
Jacob Waite <jacobwaite09@gmail.com>
2025-02-02 22:03:16 -0800

    cards now display

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
-ASSET_MANAGER.queueDownload("./PaladinSheet.png");
+ASSET_MANAGER.queueDownload("./unoCards.png");
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
-	ctx.imageSmoothingEnabled = false;
 	gameEngine.init(ctx);
-	gameEngine.addEntity(new Paladin(gameEngine,50,50, ASSET_MANAGER.getAsset("./PaladinSheet.png"), 17, 40, 16, 24, 100, 20, 10, 10));
+	new gamemanager(gameEngine);
 	gameEngine.start();
 });
